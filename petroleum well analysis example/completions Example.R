36edd12893aee71922b5f573e77cde9c7d86a6ba
Md557 <51887370+Md557@users.noreply.github.com>
2019-06-19 12:25:16 -0500

    Add files via upload

--- petroleum well analysis example/completions Example.R
+++ petroleum well analysis example/completions Example.R
@@ -1,11 +1,26 @@
-setwd("c:/users/thede/Documents/CS-code/Github")
+setwd("c:/users/thede/Documents/CS-code/Github/petroleum well analysis example")
 wb<-read.csv("final wb.csv",header= TRUE)
 wb_trun<-wb[1:64,]
 
-lin_mod<-lm(EUR.ft~BV+Mobility+xloc+yloc,data=wb_trun)
+lin_mod<-lm(EUR.ft~BVf+Mobility+xloc+yloc,data=wb_trun)
 summary(lin_mod)
 
-lin_mod1b<-lm(EUR.ft~BV+lbs.ft+xloc+yloc,data=wb_trun)
+lin_mod1b<-lm(EUR.ft~BVf+lbs.ft+xloc+yloc,data=wb_trun)
 summary(lin_mod1b)
 predictions1b<-predict(lin_mod1b,wb_trun)
 plot(wb_trun$EUR.ft,predictions1b)
+plot(lin_mod1b)
+
+lin_mod2<-lm(EUR.ft~BVf+lbs.ft+yloc,data=wb_trun)
+summary(lin_mod2)
+predictions2<-predict(lin_mod2,wb_trun)
+plot(wb_trun$EUR.ft,predictions2)
+plot(lin_mod2)
+
+
+#Change response to CFF360
+lin_mod3<-lm(CFF360~BVf+lbs.ft+xloc+yloc,data=wb_trun)
+summary(lin_mod3)
+predictions3<-predict(lin_mod3,wb_trun)
+plot(wb_trun$EUR.ft,predictions3)
+plot(lin_mod3)
